7d697dd0c0b643fe886774a1d68206f0bc030833
hethune <wenhangreg@gmail.com>
2018-06-28 00:53:27 +0800

    fix log in daemon mode

--- main.py
+++ main.py
@@ -43,27 +43,6 @@ elif log_level_config == 'ERROR':
 
 logger.setLevel(log_level)
 
-class StreamToLogger(object):
-   """
-   Fake file-like stream object that redirects writes to a logger instance.
-   """
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
-
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
-
-stdout_logger = logging.getLogger('STDOUT')
-sl = StreamToLogger(stdout_logger, logging.INFO)
-sys.stdout = sl
-
-stderr_logger = logging.getLogger('STDERR')
-sl = StreamToLogger(stderr_logger, logging.ERROR)
-sys.stderr = sl
-
 keep_fds = [handler.stream.fileno()]
 
 def action():
